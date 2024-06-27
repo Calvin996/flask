425f6671d459ac4278dbcccc172d0f8774f3c89e
Calvin996 <cheeyeong4@hotmail.com>
2024-06-27 15:57:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def index():
-  return render_template('index.html')
+def hello_world():  # put application's code here
+    return 'Hello 智勇!'
+
 
 if __name__ == '__main__':
-  app.run(port=5000)
+    app.run()
+
